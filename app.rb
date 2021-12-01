9c067937cbd1edc5c9c017f2cbf597f23103f4a8
Tomas Garcia <tomasgarciadev@gmail.com>
2021-12-01 22:54:30 +0000

    Step #20: sessions and redirect added

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle< Sinatra::Base
+  enable :sessions
+  
   configure :development do
     register Sinatra::Reloader
   end
+
+ 
 
   # get '/' do
   #   'Testing infrastructure working!'
@@ -15,8 +19,14 @@ class Battle< Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name] 
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name]  = params[:player_1_name] 
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
