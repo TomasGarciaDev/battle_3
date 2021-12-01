d5352e1f7709d4730d01548d1d6566749fb28d74
Tomas Garcia <tomasgarciadev@gmail.com>
2021-12-01 23:42:03 +0000

    Step #22: Test helper added with method

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,12 +1,7 @@
 feature 'View hit points' do
   scenario 'see player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-    
-    save_and_open_page 
-    
+    sign_in_and_play
+    # save_and_open_page 
     expect(page).to have_content 'Mittens: 60HP'
   end
 end
