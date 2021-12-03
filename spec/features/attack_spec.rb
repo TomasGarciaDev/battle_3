bac78c528bab2d2c52a498e7107ed7d1ade1c250
Tomas Garcia <tomasgarciadev@gmail.com>
2021-12-03 12:19:36 +0000

    Step #25

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,14 @@
 feature 'Attacking' do
-  scenario 'attack Player 2' do
+  # scenario 'attack Player 2' do
+  #   sign_in_and_play
+  #   click_button 'Attack'
+  #   expect(page).to have_content 'Charlotte attacked Mittens'
+  # end
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content 'Charlotte attacked Mittens'
+    # click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
   end
 end
