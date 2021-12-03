16697c8ca8021e72311f1086ffa56403ac09a7e3
riky moroni <riky5.moroni@gmail.com>
2021-12-03 17:29:38 +0000

    step 27: add initaliaze with 2 players as arguments, add globlal variable for game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
   let(:player_1) { double :player }
   let(:player_2) { double :player }
   
@@ -9,6 +9,12 @@ describe Game do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
+    end
+  end
+
+  describe '2 arguments' do
+    it 'initializes with 2 arguments' do
+      expect(Game).to respond_to(:new).with(2).argument
     end
   end
 end
